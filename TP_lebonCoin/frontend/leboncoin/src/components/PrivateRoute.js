c21e6aa1844a65610853e6b47180026bfb4f1cca
Madoff77 <e.abdenbi@ecole-ipssi.net>
2024-12-19 15:25:04 +0100

    Delete TP_lebonCoin directory

--- TP_lebonCoin/frontend/leboncoin/src/components/PrivateRoute.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token'); // Vérifie si le token JWT est présent
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
